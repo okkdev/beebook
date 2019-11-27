85dc6ca95ba515e111fb341c315e4371e9ee724d
okkdev <stehlik.jen@gmail.com>
2019-11-27 12:19:33 +0100

    add tailwindcss

--- assets/webpack.config.js
+++ assets/webpack.config.js
@@ -30,7 +30,11 @@ module.exports = (env, options) => ({
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          { loader: 'postcss-loader' }
+        ]
       }
     ]
   },
