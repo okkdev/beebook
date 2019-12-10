e7113c126054e68d3a880e5ac3270fa7ab438ed1
HailKarn <39083429+HailKarn@users.noreply.github.com>
2019-12-10 15:20:59 +0100

    dashboard inhalt

--- lib/beebook_web/controllers/dash_controller.ex
+++ lib/beebook_web/controllers/dash_controller.ex
@@ -1,7 +1,12 @@
 defmodule BeebookWeb.DashController do
   use BeebookWeb, :controller
 
+  alias Beebook.Library
+  alias Beebook.Accounts
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    users = Accounts.list_users()
+    links = Library.list_links()
+    render(conn, "index.html", users: users, links: links)
   end
 end
