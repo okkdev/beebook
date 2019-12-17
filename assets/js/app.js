cfe1682e06eb375188ffbf3b23d1a478877d558a
okkdev <stehlik.jen@gmail.com>
2019-12-17 10:56:03 +0100

    liveview fix

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,10 +13,8 @@ import "phoenix_html"
 
 // Liveview
 import {Socket} from "phoenix"
-import LiveSocket from "phoenix_live_view"
+import {LiveSocket} from "phoenix_live_view"
 
 let csrfToken = document.querySelector("meta[name='csrf-token']").getAttribute("content")
-let liveSocket = new LiveSocket("/live", {params: {_csrf_token: csrfToken}})
+let liveSocket = new LiveSocket("/live", Socket, {params: {_csrf_token: csrfToken}})
 liveSocket.connect()
-
-
