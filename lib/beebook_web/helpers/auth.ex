e3b6aa7a2a495a7e03adcdac06ac5d344ce8f6f7
okkdev <stehlik.jen@gmail.com>
2020-01-03 09:43:28 +0100

    doc and small changes

--- lib/beebook_web/helpers/auth.ex
+++ lib/beebook_web/helpers/auth.ex
@@ -1,6 +1,9 @@
 defmodule BeebookWeb.Helpers.Auth do
   import Plug.Conn, only: [get_session: 2]
 
+  @doc """
+  Returns User if logged in or nil if not.
+  """
   def signed_in?(conn) do
     !!get_session(conn, :current_user)
   end
